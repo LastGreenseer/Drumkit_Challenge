d256c203169293f032d536d327324110e56e9758
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-06-07 12:19:44 +0100

    Audio no longer loops when keys are held down

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const pressedKeys = {}; //Needed to prevent looping audio by holdong the key
+
 function playSound(e) {
+  // Prevents audio from looping if key is held
+  if (pressedKeys[e.keyCode]) return;
+  pressedKeys[e.keyCode] = true;
+
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
 
-  if (!audio) return; //This stops the function if no sound is found
+  //This stops the function if no sound is found
+  if (!audio) return;
 
-  audio.currentTime = 0; //Makes it so audio is always played from the start if key is pressed multiple times, quickly
+  //Makes it so audio is always played from the start if key is pressed multiple times, quickly
+  audio.currentTime = 0;
   audio.play();
   key.classList.add(playing); //Button animation
 }
@@ -14,7 +22,12 @@ function removeTransition(e) {
   this.classList.remove("playing");
 }
 
+function handleKeyUp(e) {
+  pressedKeys[e.keyCode] = false;
+}
+
 const keys = document.querySelectorAll("drum");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", playSound);
+window.addEventListener("keyup", handleKeyUp);
